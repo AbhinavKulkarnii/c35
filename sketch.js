6527219baf7152ce0b872e48a3cf742b6eb7e833
Abhinav Kulkarni <Abhinav@kulkarnis-mini.attlocal.net>
2020-12-30 13:47:25 -0600

    created a multiplayer game

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
 var ball;
-
+var dataBase, position;
 function setup(){
     createCanvas(500,500);
+    dataBase = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var node = dataBase.ref("ball/position");
+    node.on("value",readPosition,showError);
+
 }
+
+function readPosition(data){
+    position = data.val()
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error while reading from the database");
+}
+
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+   dataBase.ref("ball/position").set({
+       x: position.x + x,
+       y: position.y + y
+   })
+
 }
